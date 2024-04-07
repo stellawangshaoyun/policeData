a47beaa24442c448e16307e7c025e8d4abddcbbe
Stella_Wang <stella6151@gmail.com>
2024-04-07 19:59:37 +0100

    create map search bar and table components

---
+++ src/components/CrimeMap.tsx
@@ -0,0 +1,34 @@
+import mapboxgl, { Map } from "mapbox-gl";
+import React, { useEffect, useRef } from "react";
+import.meta.env.MAPBOXGL_TOKEN;
+mapboxgl.accessToken = process.env.MAPBOXGL_TOKEN
+  ? process.env.MAPBOXGL_TOKEN
+  : "";
+interface MapProps {
+  center: [number, number];
+  zoom: number;
+}
+const CrimeMap: React.FC<MapProps> = ({ center, zoom }) => {
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  const map = useRef<Map | null>(null);
+  useEffect(() => {
+    if (!map.current) {
+      map.current = new mapboxgl.Map({
+        container: mapContainerRef.current!,
+        style: "mapbox://styles/mapbox/standard",
+        center,
+        zoom,
+      });
+    }
+
+  }, [center, zoom]);
+
+  useEffect(() => {
+    if (map.current) {
+      map.current.setCenter(center);
+      map.current.setZoom(zoom);
+    }
+  }, [center, zoom]);
+  return <div ref={mapContainerRef} className="w-scrren h-screen" />;
+};
+export default CrimeMap;
---
+++ src/components/Table.tsx
@@ -0,0 +1,82 @@
+const people = [
+  {
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+  },
+  // More people...
+];
+const Table = () => {
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">
+            Users
+          </h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all the users in your account including their name, title,
+            email and role.
+          </p>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Title
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Email
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Role
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {people.map((person) => (
+                  <tr key={person.email}>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                      {person.name}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {person.title}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {person.email}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {person.role}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
